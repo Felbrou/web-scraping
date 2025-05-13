f010ec38d15d89ab41ac814264264262ebccb7a4
Felbrou <felbrou@gmail.com>
2025-05-13 17:34:21 -0300

    feat: adicionando 'seller', 'reviews_rating_number' e 'reviews_amount' ao arquivo de saída_

--- coleta/coleta/spiders/mercadolivre.py
+++ coleta/coleta/spiders/mercadolivre.py
@@ -13,5 +13,8 @@ class MercadolivreSpider(scrapy.Spider):
         for product in products:
             yield {
                 'brand': product.css("span.poly-component__brand::text").get(),
-                'name': product.css("a.poly-component__title::text").get()
+                'name': product.css("a.poly-component__title::text").get(),
+                'seller': product.css("span.poly-component__seller::text").get().replace("Por","").strip(),
+                'reviews_rating_number': product.css("span.poly-reviews__rating::text").get(),
+                'reviews_amount': product.css("span.poly-reviews__total::text").get()
             }
